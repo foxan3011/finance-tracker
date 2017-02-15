29633f7b61bfeb2a3aa20fcff0c539bfacaeb538
DATDANG <foxan3011@gmail.com>
2017-02-15 08:28:32 +0000

    devise and bootstrap for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  # before any action performed by the application controller basically any action form the UI at all we need an 
+  # authenticated user meaning a signed up user loggin 
 end
