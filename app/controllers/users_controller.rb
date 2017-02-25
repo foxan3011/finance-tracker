9f21fa13802b53b29a258f38c4023afc860bb35e
DATDANG <foxan3011@gmail.com>
2017-02-25 09:18:15 +0000

    wrap finance tracker

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,32 @@ class UsersController < ApplicationController
         @friendships = current_user.friends
     end
 
-
+    def show
+        @user = User.find(params[:id])
+        @user_stocks = @user.stocks
+    #@user_stocks = @user.user_stocks wrong it doesn't know method in stock show list
+    
+    end
+    
+    
+    def search
+        @users = User.search(params[:search_param])
+        if @users
+            @users = current_user.except_current_user(@users)
+            #bc don't wanna show up in the search
+            render partial: 'friends/lookup'
+        else
+            render status: :not_found, nothing: true
+        end
+    end    
+    def add_friend
+        @friend = User.find(params[:friend])
+        current_user.friendships.build(friend_id: @friend.id)
+    #building association (friend_id: @friend.id)
+        if current_user.save
+            redirect_to my_friends_path, notice: "Friend was successfully added"    
+        else
+            redirect_to my_friends_path, flash[:error] = "There was an error with adding user as friend"
+        end
+    end
 end
